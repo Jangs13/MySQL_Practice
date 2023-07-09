5fcb9433494b9178c2a28d55927e0ea3a3398849
Atharv Pramod Jangam <83828452+Jangs13@users.noreply.github.com>
2023-07-09 18:35:35 -0400

    Delete Lag and lead.sql

--- Lag and lead.sql
+++
@@ -1,8 +0,0 @@
-SELECT emp_no, salary,
-LAG(salary) OVER w as previous_salary,
-LEAD(salary) OVER w AS next_salary,
-salary - LAG(salary) OVER w AS Diff_current_prev_salary,
-LEAD(salary) OVER w - salary AS Diff_nex_current_salary
-FROM salaries
-WHERE emp_no BETWEEN 10500 and 10600 AND salary > 80000
-WINDOW w AS (partition by emp_no order by  salary)
